dc95c2c7afdc92ba1f9f132174d8cfd1a262a94a
Elie Khoury <elie.khoury@idiap.ch>
2014-10-09 11:00:44 +0200

    change the version to 1.1.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bob.spear',
-    version='1.1.6',
+    version='1.1.7',
     description='Speaker recognition toolkit',
     url='https://pypi.python.org/pypi/bob.spear',
     license='GPLv3',
