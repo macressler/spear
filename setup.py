cf9587440108c41f5172059bc5d932d753d54450
Elie KHOURY <elie.khoury@idiap.ch>
2015-07-17 14:28:00 +0200

    changed README

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bob.spear',
-    version='2.0.0',
+    version='1.9.0',
     description='Speaker recognition toolkit',
     url='https://pypi.python.org/pypi/bob.spear',
     license='GPLv3',
